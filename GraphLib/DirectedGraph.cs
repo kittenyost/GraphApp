0986fd2c0d37088c6979aeb14c06bb8028da4ee4
Jesse Harlan <jesse.harlan@centralia.edu>
2024-05-21 11:52:00 -0700

    Started working on directed graphs

--- GraphLib/DirectedGraph.cs
+++ GraphLib/DirectedGraph.cs
@@ -2,6 +2,16 @@
 {
     public class DirectedGraph
     {
+        public int NumVertices { get => Vertices.Count; }
+        public List<Vertex> Vertices = new List<Vertex>();
 
+        public Vertex AddVertex(string label)
+        {
+            Vertex v = new Vertex(label);
+
+            Vertices.Add(v);
+
+            return v;
+        }
     }
 }
